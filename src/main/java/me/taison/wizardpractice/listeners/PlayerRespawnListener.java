142d6c69b52bc4236c0e083737df96445166489d
Michael <michalluczak4a@gmail.com>
2022-12-12 00:58:25 +0100

    Fix piórka 3

--- src/main/java/me/taison/wizardpractice/listeners/PlayerRespawnListener.java
+++ src/main/java/me/taison/wizardpractice/listeners/PlayerRespawnListener.java
@@ -8,14 +8,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class PlayerRespawnListener implements Listener {
 
     @EventHandler
     public void handle(PlayerPostRespawnEvent event){
         event.getPlayer().teleport(WizardPractice.getSingleton().getSpawnLocation());
-        event.getPlayer().getInventory().clear();
-        event.getPlayer().getInventory().setItem(4, VariousItems.FEATHER_ITEM);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                event.getPlayer().getInventory().clear();
+                event.getPlayer().getInventory().setItem(4, VariousItems.FEATHER_ITEM);
+            }
+
+        }.runTaskLater(WizardPractice.getPlugin(WizardPractice.class), 1L);
+
         //TODO potem przekazywanie do areny  w celu dodania do spectatorów... duel.playerRespawn(user);
     }
 
